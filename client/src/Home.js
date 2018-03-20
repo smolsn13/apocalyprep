53abdeadd1abd8e6dca120c30ca878e2456f1845
Scott Molsness <smolsness@gmail.com>
2018-03-20 13:55:39 -0700

    minor updates

--- client/src/Home.js
+++ client/src/Home.js
@@ -4,20 +4,29 @@ export const Home = props => {
   return (
     <div className='row'>
       <div className='row'>
-        <div className='col s2'></div>
-        <div className='col s8'>
-          <h1 className='center-align'>Apocalyprep Home</h1>
+        <div className='col s12'>
+          <video id="background-video" loop autoPlay>
+            <source src='img/starrySky.mp4' type="video/mp4" />
+          </video>
         </div>
-        <div className='col s2'></div>
       </div>
       <div className='row'>
-        <form className='col s12'>
-          <div className='col s3'></div>
-          <div className='input-field col s6'>
-            <input id="zip" type="text" className="validate" />
-            <label for="zip">Enter your Zip Code</label>
+        <form className='col s12' action=''>
+          <div className='row'>
+            <div className='col s3'></div>
+            <div className='input-field col s4'>
+              <input placeholder='Enter your city' id='city' type='text' className='validate formInput' />
+            </div>
+            <div className='input-field col s2'>
+              <input placeholder='State' id='state' type='text' className='validate formInput' />
+            </div>
+            <div className='col s3'></div>
           </div>
-          <div className='col s3'></div>
+          <div className='row'>
+            <div className='col s5'></div>
+              <i className="material-icons md-48">navigate_next</i>
+            <div className='col s5'></div>
+          </div>
         </form>
       </div>
     </div>
